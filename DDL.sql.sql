977edc092cb9145721ab8ce46b3ef6df999e354d
devlucas <lucasalbino@gmail.com>
2022-09-26 21:14:13 -0300

    Todo6

---
+++ DDL.sql.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS todo5;
+USE todo5;
+
+CREATE TABLE Produtos(
+id int(4),
+nome varchar(30),
+tipo varchar (10),
+genero varchar (10),
+tamanho varchar (1),
+cor varchar (10),
+tecido varchar (10)
+);
---
+++ DML-Updates.sql
@@ -0,0 +1,19 @@
+UPDATE  Produtos 
+SET tipo='calca'
+WHERE nome='calca jens';
+
+UPDATE  Produtos 
+SET tamanho='38'
+WHERE nome='calca jens';
+
+UPDATE  Produtos 
+SET tecido='jeans-azul'
+WHERE nome='calca jens';
+
+UPDATE  Produtos 
+SET cor='preta'
+WHERE nome='calca jens';
+
+UPDATE  Produtos 
+SET tecido='jeans-azul'
+WHERE nome='calca jens';
